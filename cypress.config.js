e1b9a95f00fa79171dfcf0fa9849a455dd6054db
Aasimshaik08 <aasim.s.k0809@gmail.com>
2026-04-11 02:00:38 +0530

    CI workflow
    removed unnessary files

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,24 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'cypress/results/results-[hash].xml',
+    toConsole: true,
+  },
+
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
     video: false,
     screenshotOnRunFailure: true,
     defaultCommandTimeout: 10000,
+
     env: {
       email: process.env.CYPRESS_EMAIL,
       password: process.env.CYPRESS_PASSWORD,
       apiUrl: process.env.CYPRESS_API_URL || 'http://localhost:5000/api',
     },
+
     setupNodeEvents(on, config) {
       return config;
     },
